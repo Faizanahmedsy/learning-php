aa17a9efc0647c3e8d3ae8d170b6f698ce4577f8
Faizanahmed Saiyed <119733178+Faizanahmedsy@users.noreply.github.com>
2024-01-15 17:42:50 +0530

    learned Get and Post api

---
+++ form.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+    <title>Faby</title>
+    </head>
+
+
+    <body>
+        <form action="backend.php" method="post">
+    Name: <input type="text" name="fname"><br><br>
+    Age: <input type="text" name="age"> <br> <br>
+
+
+    <input type="submit" name="save">
+
+        </form>
+    </body>
+</html>
